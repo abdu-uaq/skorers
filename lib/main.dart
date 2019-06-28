d78bb65fee5360a8d32c92e7f51c006bcfd507f3
Luca Cavallin <me@lucavall.in>
2019-06-28 13:25:23 +0200

    Display score list

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,50 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Skorers',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Skorers'),
-        ),
-        body: Center(
-          child: Text('Skorers'),
-        ),
+      home: Scores()
+    );
+  }
+}
+
+class ScoresState extends State<Scores> {
+  final _scores = <String>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Skorers'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+          if (index >= _scores.length) {
+            _scores.add("Luca, Tim  vs Giorgio, Reiner: 3-0");
+          }
+          return _buildRow(_scores[index]);
+        });
+  }
+
+  Widget _buildRow(String score) {
+    return ListTile(
+      title: Text(
+        score,
+        style: _biggerFont,
       ),
     );
   }
+}
+
+class Scores extends StatefulWidget {
+  @override
+  ScoresState createState() => ScoresState();
 }
